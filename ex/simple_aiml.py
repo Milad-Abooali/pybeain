0e04b2a2f2e118e8129471f4d67deb538a274ca9
Milad.A <m.abooali@hotmail.com>
2021-02-24 14:51:35 +0300

    PY2PHP

--- ex/simple_aiml.py
+++ ex/simple_aiml.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import aiml
+import time
 
-alice = aiml.Kernel()
-alice.learn("std-startup.xml")
-alice.respond("LOAD AIML")
+airob = aiml.Kernel()
+airob.learn("std-startup.xml")
+airob.respond("LOAD AIML")
 
+lastInput = ''
 while True:
-   print (alice.respond(input("> ")))
-   
+    inputFile  = open('temp/input.txt', 'r')
+    inputLines = inputFile.readline()
+    if lastInput!=inputLines:
+        lastInput = inputLines
+        outputFile = open('temp/output.txt', "w")
+        print (inputLines)
+        output = airob.respond(inputLines)
+        print ('res:'+output)
+        outputFile.write(output)
+        outputFile.write("\n")
+        outputFile.close()
+        time.sleep(3)
+    print ('...')
+    time.sleep(2)
+    inputFile.close()
+
