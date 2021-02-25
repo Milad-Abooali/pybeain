8a0783c57b410f5120524d0b228ee7516071b070
Milad.A <m.abooali@hotmail.com>
2021-02-25 14:08:43 +0300

    Bridge

--- ex/simple_aiml.py
+++ ex/simple_aiml.py
@@ -11,18 +11,19 @@ airob.learn("brain\\ex\\std-startup.xml")
 airob.respond("LOAD AIML")
 
 lastInput = ''
-#print (os.getcwdb())
 
-oFilePath = 'brain\\ex\\temp\\'+sys.argv[1] + '_output.txt'
-iFilePath = 'brain\\ex\\temp\\'+sys.argv[1] + '_input.txt'
+oFilePath = 'brain\\ex\\temp\\'+sys.argv[1] + '\\response.log'
+iFilePath = 'brain\\ex\\temp\\'+sys.argv[1] + '\\input.log'
 
-print(os.getpid());
+pidFilePath = 'brain\\ex\\temp\\'+sys.argv[1] + '\\pid.log'
+outputFile = open(pidFilePath, "w")
+outputFile.write(os.getpid())
+outputFile.close()
+
 i=0
 while True:
-    if i>10:
-        outputFile = open(oFilePath, "w")
-        outputFile.write('#~50')
-        outputFile.close()
+    if i>1500:
+        os.remove(pidFilePath)
         break
     else:
         inputFile  = open(iFilePath, 'r')
